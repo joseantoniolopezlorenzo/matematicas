eba4ebd057d8d870a805ed6acc4533ec5f283966
José Antonio López Lorenzo <jall.informatico@gmail.com>
2021-06-07 06:54:29 +0200

    minima modificacion

--- assets/js/graficos/eso4-t1.js
+++ assets/js/graficos/eso4-t1.js
@@ -1,112 +1,115 @@
-(function () {
-  JXG.Options.text.useMathJax = true;
-  JXG.Options.text.useMathJax.display = 'html';
-  
-  const createBoard = function (id) {
-    return JXG.JSXGraph.initBoard(id, {
-      boundingbox: [-11, 1, 11, -1],
-      axis: false,
-      showCopyright: false,
-      showNavigation: false,
-    });
-  };
+;
+(function(JXG) {
+    JXG.Options.text.useMathJax = true;
+    JXG.Options.text.useMathJax.display = 'html';
 
-  const createCeroEje = function (board) {
-    board.create("point", [0.0, 0.0], {
-      face: "+",
-      name: "0",
-      strokeColor: "black",
-      size: 2,
-      fixed: true,
-    });
-    board.create(
-      "line",
-      [
-        [-10, 0],
-        [10, 0],
-      ],
-      { strokeColor: "red", fixed: true }
-    );
-    board.create("point", [-10.7, 0], {
-      size: 0,
-      name: "\\[-\\infty\\]",
-    });
-    board.create("point", [9, 0], {
-      size: 0,
-      name: "\\[+\\infty\\]",
-    });
-  };
-
-  const dibujaExtremo = function (tipo, nombre) {
-    let color = "";
-    if (tipo === "a") {
-      color = "white";
-    } else {
-      color = "black";
-    }
-    return {
-      strokeColor: "black",
-      fillColor: color,
-      name: nombre,
-      size: 3,
-      fixed: true,
+    const createBoard = function(id) {
+        return JXG.JSXGraph.initBoard(id, {
+            boundingbox: [-11, 1, 11, -1],
+            axis: false,
+            showCopyright: false,
+            showNavigation: false,
+        });
     };
-  };
-  // INTERVALO ABIERTO
-  const intervalo1 = createBoard("intervalo1");
-  createCeroEje(intervalo1);
 
-  const p1 = intervalo1.create("point", [-5, 0], dibujaExtremo("a", '-5'));
-  const p2 = intervalo1.create("point", [2, 0], dibujaExtremo("a", '2'));
-  intervalo1.create("segment", [p1, p2]);
+    const createCeroEje = function(board) {
+        board.create("point", [0.0, 0.0], {
+            face: "+",
+            name: "0",
+            strokeColor: "black",
+            size: 2,
+            fixed: true,
+        });
+        board.create(
+            "line",
+            [
+                [-10, 0],
+                [10, 0],
+            ], {
+                strokeColor: "red",
+                fixed: true
+            }
+        );
+        board.create("point", [-10.7, 0], {
+            size: 0,
+            name: "\\[-\\infty\\]",
+        });
+        board.create("point", [9, 0], {
+            size: 0,
+            name: "\\[+\\infty\\]",
+        });
+    };
 
-  // INTERVALO CERRADO
-  const intervalo2 = createBoard("intervalo2");
-  createCeroEje(intervalo2);
+    const dibujaExtremo = function(tipo, nombre) {
+        let color = "";
+        if (tipo === "a") {
+            color = "white";
+        } else {
+            color = "black";
+        }
+        return {
+            strokeColor: "black",
+            fillColor: color,
+            name: nombre,
+            size: 3,
+            fixed: true,
+        };
+    };
+    // INTERVALO ABIERTO
+    const intervalo1 = createBoard("intervalo1");
+    createCeroEje(intervalo1);
 
-  const p21 = intervalo2.create("point", [-5.0, 0.0], dibujaExtremo('b', '-5'));
-  const p22 = intervalo2.create("point", [2.0, 0.0], dibujaExtremo('b', '2'));
-  intervalo2.create("segment", [p21, p22]);
+    const p1 = intervalo1.create("point", [-5, 0], dibujaExtremo("a", '-5'));
+    const p2 = intervalo1.create("point", [2, 0], dibujaExtremo("a", '2'));
+    intervalo1.create("segment", [p1, p2]);
 
-  // INTERVALO SEMIABIERTTO
-  const intervalo3 = createBoard("intervalo3");
-  createCeroEje(intervalo3);
+    // INTERVALO CERRADO
+    const intervalo2 = createBoard("intervalo2");
+    createCeroEje(intervalo2);
 
-  const p31 = intervalo3.create("point", [-5.0, 0.0], dibujaExtremo('b', '-5'));
-  const p32 = intervalo3.create("point", [2.0, 0.0], dibujaExtremo('a', '2'));
-  intervalo3.create("segment", [p31, p32]);
+    const p21 = intervalo2.create("point", [-5.0, 0.0], dibujaExtremo('b', '-5'));
+    const p22 = intervalo2.create("point", [2.0, 0.0], dibujaExtremo('b', '2'));
+    intervalo2.create("segment", [p21, p22]);
 
-  const intervalo4 = createBoard("intervalo4");
-  createCeroEje(intervalo4);
+    // INTERVALO SEMIABIERTTO
+    const intervalo3 = createBoard("intervalo3");
+    createCeroEje(intervalo3);
 
-  const p40 = intervalo4.create("point", [-5, 0], dibujaExtremo('a', '-5'));
-  const p42 = intervalo4.create("point", [2, 0], dibujaExtremo('b', '2'));
-  intervalo4.create("segment", [p40, p42]);
+    const p31 = intervalo3.create("point", [-5.0, 0.0], dibujaExtremo('b', '-5'));
+    const p32 = intervalo3.create("point", [2.0, 0.0], dibujaExtremo('a', '2'));
+    intervalo3.create("segment", [p31, p32]);
 
-  // SEMIRECTA 1
-  const semirecta1 = createBoard("semirecta1");
-  createCeroEje(semirecta1);
+    const intervalo4 = createBoard("intervalo4");
+    createCeroEje(intervalo4);
 
-  const p41 = semirecta1.create("point", [2.0, 0.0], dibujaExtremo('a', 'a'));
-  semirecta1.create("arrow", [p41, [-10, 0]]);
+    const p40 = intervalo4.create("point", [-5, 0], dibujaExtremo('a', '-5'));
+    const p42 = intervalo4.create("point", [2, 0], dibujaExtremo('b', '2'));
+    intervalo4.create("segment", [p40, p42]);
 
-  // SEMIRECTA 2
-  const semirecta2 = createBoard("semirecta2");
-  createCeroEje(semirecta2);
+    // SEMIRECTA 1
+    const semirecta1 = createBoard("semirecta1");
+    createCeroEje(semirecta1);
 
-  const p51 = semirecta2.create("point", [5.0, 0.0],dibujaExtremo('b', 'a'));
-  semirecta2.create("arrow", [p51, [-10, 0]]);
+    const p41 = semirecta1.create("point", [2.0, 0.0], dibujaExtremo('a', 'a'));
+    semirecta1.create("arrow", [p41, [-10, 0]]);
 
-  // SEMIRECTA 3
-  const semirecta3 = createBoard("semirecta3");
-  createCeroEje(semirecta3);
-  const p61 = semirecta3.create("point", [1.0, 0.0], dibujaExtremo('a', 'a'));
-  semirecta3.create("arrow", [p61, [10, 0]]);
+    // SEMIRECTA 2
+    const semirecta2 = createBoard("semirecta2");
+    createCeroEje(semirecta2);
 
-  // SEMIRECTA 4
-  const semirecta4 = createBoard("semirecta4");
-  createCeroEje(semirecta4);
+    const p51 = semirecta2.create("point", [5.0, 0.0], dibujaExtremo('b', 'a'));
+    semirecta2.create("arrow", [p51, [-10, 0]]);
 
-  const p71 = semirecta4.create("point", [-2.0, 0.0], dibujaExtremo('b', 'a'));
-  semirecta4.create("arrow", [p71, [10, 0]]);
-})();
+    // SEMIRECTA 3
+    const semirecta3 = createBoard("semirecta3");
+    createCeroEje(semirecta3);
+    const p61 = semirecta3.create("point", [1.0, 0.0], dibujaExtremo('a', 'a'));
+    semirecta3.create("arrow", [p61, [10, 0]]);
+
+    // SEMIRECTA 4
+    const semirecta4 = createBoard("semirecta4");
+    createCeroEje(semirecta4);
+
+    const p71 = semirecta4.create("point", [-2.0, 0.0], dibujaExtremo('b', 'a'));
+    semirecta4.create("arrow", [p71, [10, 0]]);
+})(JXG);
--- src/eso-4/tema-1/teoria.md
+++ src/eso-4/tema-1/teoria.md
@@ -1,5 +1,5 @@
 ---
-title: 4º ESO - Tema 1
+title: Tema 1
 subtitle: Los números reales
 author: JallProfesor
 date: 01/02/2021
@@ -17,45 +17,45 @@ script: ../../assets/js/graficos/eso4-t1.js
 ---
 - [Ejercicios resueltos](./resueltos.html)
 - [Indice general](/index.html)
-  
+
 # 1. Conjuntos de números
 ****
 ## CURSOS ANTERIORES.
 
-Conujuntos numéricos que ya has estudiado:[^nota1]
+Conjuntos numéricos que ya has estudiado:[^nota1]
 
 * **NATURALES** $\mathbb N$: $1, 2, 3, \sqrt[3]{4}, 2^{0}, \sqrt{16}, 3^{1}, 4^{2}, ...$
 * **ENTEROS** $\mathbb Z$: $\mathbb N \cup \{0, -1, -2, ...\}$
-* **RACIONALES** $\mathbb Q$ \ 
-  Números racionales son los que se pueden poner como *cociente* de dos números enteros. Su expresión decimal puede ser *exacta* ($\frac{3}{4} = 0,75$ ) o *periódica* ($\frac{1}{3} = 0,\overset{\frown}{3}$). 
+* **RACIONALES** $\mathbb Q$ \
+  Números racionales son los que se pueden poner como *cociente* de dos números enteros. Su expresión decimal puede ser *exacta* ($\frac{3}{4} = 0,75$ ) o *periódica* ($\frac{1}{3} = 0,\overset{\frown}{3}$).
 
   [^nota1]:
   **Se cumple** \
-  $$\mathbb{Q} = \mathbb Z \cup \left\{ \frac{1}{2}, \frac{2}{1}, -\frac{1}{2}, ...\right\}$$ 
+  $$\mathbb{Q} = \mathbb Z \cup \left\{ \frac{1}{2}, \frac{2}{1}, -\frac{1}{2}, ...\right\}$$
   $$\mathbb N \subset \mathbb Z \subset \mathbb Q$$
 
 
 ## LOS IRRACIONALES.
 
 Son **números irracionales** ($\mathbb I$) aquellos números que no son racionales y por tanto no
-pueden expresarse como fracción de números enteros. 
+pueden expresarse como fracción de números enteros.
 
-En otras palabras, son todos aquellos números que tienen una *expresión decimal que no es ni exacta ni periódica*, es decir, infinitas cifras decimales y sin periodo. 
+En otras palabras, son todos aquellos números que tienen una *expresión decimal que no es ni exacta ni periódica*, es decir, infinitas cifras decimales y sin periodo.
 
 Por ejemplo: $\sqrt{2},~\sqrt{3},~\pi,~e, ~\varphi ~ \text{(proporción áurea)}$
 
 
 ## LOS REALES.
 Los **NÚMEROS REALES** son la unión de los *racionales* y los *irracionales* [^nota2]
-$$\mathbb R = \mathbb Q \cup \mathbb I$$ 
+$$\mathbb R = \mathbb Q \cup \mathbb I$$
 
-**Recta real** 
+**Recta real**
 es una recta en la que elegimos el origen de coordenadas y el tamaño de la unidad\
 (colocamos el 0 y el 1). Decimos que es **real** por que:
 
-- todo **número real** ocupa una **posición** en la recta numérica. 
+- todo **número real** ocupa una **posición** en la recta numérica.
 - todo **punto de la recta** se puede **hacer corresponder** con un número real.
-  
+
 [^nota2]:
 **Propiedad importante:** Los números reales son **DENSOS**, es decir, entre dos cualesquiera números reales hay *infinitos números* en medio.Eso es fácil de entender, si $a, b$ son dos números con $a < b$ tenemos que $$a < \frac {a + b}{2} <b $$ es decir, la *media* está entre los dos números. Y esto podemos hacerlo las veces que queramos (infinitas veces). Los racionales y los irracionales son también *densos*.
 
@@ -63,18 +63,18 @@ es una recta en la que elegimos el origen de coordenadas y el tamaño de la unid
 ****
 ## DEFINICIONES.
 
-### **Intervalo** 
+### **Intervalo**
 es el conjunto de los números reales que hay entre dos números dados que llamamos **extremos**.
 
-### **Semirecta** 
+### **Semirecta**
 es el conjunto de números reales que no está limitado por uno de sus extremos.
 
 ## TIPOS DE INTERVALOS.
 
-### **Abiertos** 
-    
+### **Abiertos**
+
 conjunto de números reales que hay entre dos dados **excluyendo** los extremos. \
-    Por ejemplo: $(-5,2) = \{x | -5 < x < 2\}$ 
+    Por ejemplo: $(-5,2) = \{x | -5 < x < 2\}$
 
 ::: intervalo
 <div id="intervalo1" class="jxgbox" style="width:100%; height:100%;"></div>
@@ -82,7 +82,7 @@ conjunto de números reales que hay entre dos dados **excluyendo** los extremos.
 
 ### **Cerrados **
 conjunto de números reales que hay entre dos dados **incluyendo** los extremos. \
-    Por ejemplo: $[-5,2] = \{x | -5 \leq x \leq 2\}$ 
+    Por ejemplo: $[-5,2] = \{x | -5 \leq x \leq 2\}$
 
 ::: intervalo
 <div id="intervalo2" class="jxgbox" style="width:100%; height:100%;"></div>
@@ -90,13 +90,13 @@ conjunto de números reales que hay entre dos dados **incluyendo** los extremos.
 
 ### **Semiabiertos**
  conjunto de números reales que hay entre dos dados **incluyendo** sólo uno de los extremos. \
- Por ejemplo: $[-5,2) = \{x | -5 \leq x < 2\}$ 
+ Por ejemplo: $[-5,2) = \{x | -5 \leq x < 2\}$
 
 ::: intervalo
 <div id="intervalo3" class="jxgbox" style="width:100%; height:100%;"></div>
 :::
 
-Por ejemplo: $(-5,2] = \{x | -5 < x \leq 2\}$ 
+Por ejemplo: $(-5,2] = \{x | -5 < x \leq 2\}$
 
 ::: intervalo
 <div id="intervalo4" class="jxgbox" style="width:100%; height:100%;"></div>
@@ -114,21 +114,21 @@ Por ejemplo: $(-5,2] = \{x | -5 < x \leq 2\}$
 
 - conjunto de todos los menores que $a$ y el mismo $a$ (incluido $a$)
   $$(-\infty,a] = \{x | x \leq a\}$$
-  
+
 ::: intervalo
 <div id="semirecta2" class="jxgbox" style="width:100%; height:100%"></div>
 :::
 
 - conjunto de todos los mayores que $a$
   $$(a, +\infty) = \{x | x > a\}$$
-  
+
 ::: intervalo
 <div id="semirecta3" class="jxgbox" style="width:100%; height:100%"></div>
 :::
 
 - conjunto de todos los menores que $a$ y el mismo $a$ (incluido $a$)
   $$[a, +\infty) = \{x | x \geq a\}$$
-  
+
 ::: intervalo
 <div id="semirecta4" class="jxgbox" style="width:100%; height:100%"></div>
 :::
@@ -139,7 +139,7 @@ Por ejemplo: $(-5,2] = \{x | -5 < x \leq 2\}$
 
 ### Exponente entero
 
-Dado $a$, un número cualquiera, y $n$, un número *natural* ($n \in \mathbb N$), **la potencia $a^n$** es el producto del número $a$ por sí mismo $n$ veces. $a$ se llama la **base** y $n$, el **exponente**. Se dice que $a$ está **elevado** a $n$. 
+Dado $a$, un número cualquiera, y $n$, un número *natural* ($n \in \mathbb N$), **la potencia $a^n$** es el producto del número $a$ por sí mismo $n$ veces. $a$ se llama la **base** y $n$, el **exponente**. Se dice que $a$ está **elevado** a $n$.
 
 Es **importante** recordar:
 
